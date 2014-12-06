6201fee80bfb0bc26491c61e11c002c64d999073
Chris Barker <chris.a.barker@icloud.com>
2014-12-05 22:39:22 -0500

    Top menu changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    @page = {:title => "Home", :head_title => "Home"}
+    @page = {:title => "Welcome!", :head_title => "Home"}
   end
   
 end
